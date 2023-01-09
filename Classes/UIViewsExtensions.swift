2e04fd65eae3dd032fe065c5ada98ce244549e69
Oz Shabbat <ozshabbat@Ozs-MacBook-Pro.local>
2023-01-09 13:25:16 +0200

    s

--- Classes/UIViewsExtensions.swift
+++ Classes/UIViewsExtensions.swift
@@ -406,7 +406,6 @@ extension UIView {
     public func updateHeightConstraint(newHeight: CGFloat,
                                        animateInterval: TimeInterval = 0.0,
                                        _ completion: (() -> Void)? = nil) {
-        
         var heightConstr = getHeightConstraint()
         if heightConstr == nil {
             heightConstr = setHeight(height: 0)
